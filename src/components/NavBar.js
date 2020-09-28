63e799a7f2090a556b114d984e51ebbc56e23bd2
otimthomas <totim444@hotmail.co.uk>
2020-09-28 17:21:47 +0300

    nav toggle function fix

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -6,44 +6,41 @@ import './NavBar.css';
 function NavBar() {
   const [isOpen, setIsOpen] = useState(false);
 
-  // React.useEffect(() => {
-  //   setIsOpen(true)
-  // }, [isOpen])
-
-
   const toggle = () => setIsOpen(!isOpen);
+
+  const closeNav = () => setIsOpen(false);
   return (
     <Navbar color='light' light expand='md' sticky='top'>
       <NavbarToggler onClick={toggle} />
       <Collapse isOpen={isOpen} navbar>
         <Nav className='mr-auto' navbar>
           <NavItem>
-            <NavLink onClick={toggle} exact activeClassName='selected' to='/'>
+            <NavLink onClick={closeNav} exact activeClassName='selected' to='/'>
               Home
             </NavLink>
           </NavItem>
           <NavItem>
-            <NavLink onClick={toggle} activeClassName='selected' to='/about'>
+            <NavLink onClick={closeNav} activeClassName='selected' to='/about'>
               About us
             </NavLink>
           </NavItem>
           <NavItem>
-            <NavLink onClick={toggle} activeClassName='selected' to='/services'>
+            <NavLink onClick={closeNav} activeClassName='selected' to='/services'>
               Services
             </NavLink>
           </NavItem>
           <NavItem>
-            <NavLink onClick={toggle} activeClassName='selected' to='/gallery'>
+            <NavLink onClick={closeNav} activeClassName='selected' to='/gallery'>
               Gallery
             </NavLink>
           </NavItem>
           <NavItem>
-            <NavLink onClick={toggle} activeClassName='selected' to='/team'>
+            <NavLink onClick={closeNav} activeClassName='selected' to='/team'>
               Team
             </NavLink>
           </NavItem>
           <NavItem>
-            <NavLink onClick={toggle} activeClassName='selected' to='/contact'>
+            <NavLink onClick={closeNav} activeClassName='selected' to='/contact'>
               Contact Us
             </NavLink>
           </NavItem>
